77b29bf41702ceec4bcdae27a75bf77f99c9b376
Ebba Hansson <ebha2100@student.miun.se>
2022-09-08 13:49:22 +0200

    game table created

---
+++ database/migrations/2022_09_08_113223_create_games_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Namn
+            $table->integer('hours'); // Timmar spelat
+            $table->boolean('finished'); // Klarat spelet eller ej
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+};
